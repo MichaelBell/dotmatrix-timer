99115932710c961aace9b28d6a76d943364b302c
Mike Bell <mike@mercuna.com>
2022-08-31 23:38:11 +0100

    Dot Matrix timer - initial commit

---
+++ clocks.c
@@ -0,0 +1,99 @@
+#include "hardware/clocks.h"
+#include "hardware/regs/clocks.h"
+#include "hardware/watchdog.h"
+#include "hardware/pll.h"
+#include "hardware/vreg.h"
+#include "hardware/xosc.h"
+#include "hardware/structs/rosc.h"
+#include "hardware/structs/syscfg.h"
+#include "hardware/structs/xip_ctrl.h"
+
+void custom_clocks_init()
+{
+    // Reduce voltage to 0.95 and reduce brownout detection voltage to 
+    // avoid tripping it due to small fluctuations.
+#if 1
+    hw_write_masked(&vreg_and_chip_reset_hw->bod, 0b1000 << VREG_AND_CHIP_RESET_BOD_VSEL_LSB, VREG_AND_CHIP_RESET_BOD_VSEL_BITS);
+    vreg_set_voltage(VREG_VOLTAGE_0_95);
+#else
+    // This version reduces voltage to 0.80V
+    // This works for me but is quite a long way below spec!
+    hw_write_masked(&vreg_and_chip_reset_hw->bod, 0b0100 << VREG_AND_CHIP_RESET_BOD_VSEL_LSB, VREG_AND_CHIP_RESET_BOD_VSEL_BITS);
+    hw_write_masked(&vreg_and_chip_reset_hw->vreg, 0b0101 << VREG_AND_CHIP_RESET_VREG_VSEL_LSB, VREG_AND_CHIP_RESET_VREG_VSEL_BITS);
+
+    // Change ROSC divider to 5 to give a clock around 10MHz
+    // (guaranteed between 3.6 and 24MHz).
+    rosc_hw->div = 0xaa0 + 5;
+#endif
+
+    // Start tick in watchdog
+    watchdog_start_tick(XOSC_MHZ * 4);
+
+    // Disable resus that may be enabled from previous software
+    clocks_hw->resus.ctrl = 0;
+
+    xosc_init();
+
+    // Before we touch PLLs, switch sys and ref cleanly away from their aux sources.
+    hw_clear_bits(&clocks_hw->clk[clk_sys].ctrl, CLOCKS_CLK_SYS_CTRL_SRC_BITS);
+    while (clocks_hw->clk[clk_sys].selected != 0x1)
+        tight_loop_contents();
+    hw_clear_bits(&clocks_hw->clk[clk_ref].ctrl, CLOCKS_CLK_REF_CTRL_SRC_BITS);
+    while (clocks_hw->clk[clk_ref].selected != 0x1)
+        tight_loop_contents();
+
+    // Configure clocks
+    clock_configure(clk_ref,
+                    CLOCKS_CLK_REF_CTRL_SRC_VALUE_XOSC_CLKSRC,
+                    0, // No aux mux
+                    XOSC_MHZ * MHZ,
+                    XOSC_MHZ * MHZ);
+
+  clock_configure(clk_sys,
+      CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLKSRC_CLK_SYS_AUX,
+      CLOCKS_CLK_SYS_CTRL_AUXSRC_VALUE_XOSC_CLKSRC,
+      XOSC_MHZ * MHZ,
+      (XOSC_MHZ * MHZ) / 24);
+
+  clock_configure(clk_peri,
+      0,
+      CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_XOSC_CLKSRC,
+      XOSC_MHZ * MHZ,
+      (XOSC_MHZ * MHZ) / 12);
+
+#if 0
+  clock_configure(clk_adc,
+      0,
+      CLOCKS_CLK_ADC_CTRL_AUXSRC_VALUE_XOSC_CLKSRC,
+      XOSC_MHZ * MHZ,
+      XOSC_MHZ * MHZ);
+#endif
+
+  clock_configure(clk_rtc,
+      0,
+      CLOCKS_CLK_RTC_CTRL_AUXSRC_VALUE_XOSC_CLKSRC,
+      XOSC_MHZ * MHZ,
+      46875);
+
+  // The PLLs shouldn't be initialized, but this does no harm
+  pll_deinit(pll_sys);
+  pll_deinit(pll_usb);
+
+#if 1
+#if PICO_COPY_TO_RAM
+  // Shutdown unused memory banks
+  syscfg_hw->mempowerdown = 0x58;
+
+  // And disable unused clock regions
+  clocks_hw->wake_en1 = 0x303e;
+  clocks_hw->wake_en0 = 0x73ef0f3f;
+#else
+  // Shutdown unused memory banks
+  syscfg_hw->mempowerdown = 0x5e;
+
+  // And disable unused clock regions
+  clocks_hw->wake_en1 = 0x703e;
+  clocks_hw->wake_en0 = 0x10ef0f7f;
+#endif
+#endif
+}
--- main.cpp
+++ main.cpp
@@ -1,18 +1,72 @@
-#include "pico_explorer.hpp"
+#include <math.h>
+#include <string>
+
+#include "hardware/pwm.h"
+#include "hardware/pll.h"
+#include "hardware/clocks.h"
+
+#include "breakout_dotmatrix.hpp"
+#include "time.h"
+
+// This allows underclocking CLK_REF to allow a slower sys clk
+#define TIME_MULT 4
 
 using namespace pimoroni;
 
-uint16_t buffer[PicoExplorer::WIDTH * PicoExplorer::HEIGHT];
-PicoExplorer pico_explorer(buffer);
-
 int main() {
-    pico_explorer.init();
+  I2C i2c(BOARD::BREAKOUT_GARDEN);
+  BreakoutDotMatrix display(&i2c);
 
-    pico_explorer.set_pen(255, 0, 0);
+  bool led_toggle = false;
 
-    while(true) {
-        pico_explorer.pixel(Point(0, 0));
-        // now we've done our drawing let's update the screen
-        pico_explorer.update();
+  pwm_config cfg = pwm_get_default_config();
+  pwm_init(pwm_gpio_to_slice_num(PICO_DEFAULT_LED_PIN), &cfg, true);
+  pwm_set_wrap(pwm_gpio_to_slice_num(PICO_DEFAULT_LED_PIN), 255);
+  pwm_set_gpio_level(PICO_DEFAULT_LED_PIN, 0);
+  gpio_set_function(PICO_DEFAULT_LED_PIN, GPIO_FUNC_PWM);
+
+  display.init();
+  display.show();
+
+  while(true) {
+    absolute_time_t at = get_absolute_time();
+    uint64_t t = to_us_since_boot(at) * TIME_MULT;
+
+    std::string count;
+    if (t < 100000000) {
+        count = std::to_string((t / 1000000) % 100);
     }
+    else if (t < 600000000) {
+        display.set_decimal(false, true);
+        count = std::to_string(t / 6000000);
+    }
+    else {
+        display.set_decimal(false, false);
+        count = std::to_string(t / 60000000);
+    }
+
+    uint16_t left, right;
+    if(count.length() == 1) {
+        left = '0';
+        right = count[0];
+    }
+    else {
+        left = count[0];
+        right = count[1];
+    }
+
+    display.set_character(0, left);
+    display.set_character(5, right);
+    display.show();
+
+    pwm_set_gpio_level(PICO_DEFAULT_LED_PIN, led_toggle ? 64 : 0);
+    led_toggle = !led_toggle;
+
+    at = get_absolute_time();
+    t = to_us_since_boot(at) * TIME_MULT;
+    uint64_t w = t / 500000;
+    w = (w + 1) * 500000 - t;
+    
+    sleep_us(w / TIME_MULT);
+  }
 }
